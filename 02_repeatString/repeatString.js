7f9a513253552484ca43b32e7dc7fabb8b84d4ab
Vigilance <152765003+vigilancebrandon@users.noreply.github.com>
2024-01-08 16:42:16 -0500

    Solve 02_repeatString for all 7 tests
    Added 'ERROR' as return for negative inputs.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,14 @@
 const repeatString = function(string, number) {
   let newstring = ''
-  for (i = 0; i < number; i++) {
+  if (number < 0) {
+    return 'ERROR'
+  }
+  for (let i = 0; i < number; i++) {
     newstring += string
   };
   return newstring;
 };
+
+console.log(repeatString('hey', -1));
 // Do not edit below this line
 module.exports = repeatString;
