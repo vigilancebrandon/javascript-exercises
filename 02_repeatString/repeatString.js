21cd3d12b1e5d78d166fc757851e32ebd26053f9
Vigilance <152765003+vigilancebrandon@users.noreply.github.com>
2024-01-08 15:50:35 -0500

    Solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+  let newstring = ''
+  for (i = 0; i < number; i++) {
+    newstring += string
+  };
+  return newstring;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
