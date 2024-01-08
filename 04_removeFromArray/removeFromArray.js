fc2b144a911d77a9e06ff9b20884f7933dcfac43
Vigilance <152765003+vigilancebrandon@users.noreply.github.com>
2024-01-08 17:26:38 -0500

    Add semi-successful function to 04_removeFromArray
    The code works if items to remove are passed as an array. Still needs
    code to accept an arbitrary number of arguments after array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, items) {
+  let newArray = array
+  for (const item of items) {
+      array.splice(array.indexOf(item), 1)
+      //console.log(newArray)
+    }
+  return array;
+}
 
-};
-
+console.log(removeFromArray([1, 2, 3, 4]))
 // Do not edit below this line
 module.exports = removeFromArray;
+
+//Take array and items
+//Make variable newArray equal to array
+//Loop through items
+//Loop through elements in array
+//If element is strictly equal to item
+//Remove element from newArray
+//Return newArray
